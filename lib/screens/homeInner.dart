b9f7fbc37a9e62400d38f69fef9d2c2d92b57487
ronit <ronit@sambuq.com>
2023-07-04 00:37:35 +0530

    Google Fonts Added

--- lib/screens/homeInner.dart
+++ lib/screens/homeInner.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ott_app/assets.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeInner extends StatefulWidget {
   const HomeInner({super.key});
@@ -19,6 +20,16 @@ class _HomeInnerState extends State<HomeInner> {
           width: double.infinity,
           child: Image.asset(Assets.strangerthings),
         ),
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Text(
+            'Trending',
+            style: GoogleFonts.poppins(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+        )
       ],
     );
   }
